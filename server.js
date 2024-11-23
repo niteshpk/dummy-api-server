780a9146d6d37e92b469c39b17b520e5e02066be
Nitesh Kesarkar <niteshkesarkar136@gmail.com>
2024-11-23 07:44:33 +0530

    readable file access

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+const path = require("path");
+const os = require("os");
+const dbPath = path.join(os.tmpdir(), "db.json");
+
+// Ensure db.json is copied to /tmp
+const fs = require("fs");
+fs.copyFileSync("./db.json", dbPath);
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(dbPath);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-  console.log("JSON Server is running");
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
